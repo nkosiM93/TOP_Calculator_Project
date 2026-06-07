58309453dccf5d2d63075e00c23f86049c3d2525
Mathiya <mathiyathokozanin@gmail.com>
2026-06-07 16:22:50 +0200

    Added button and button labels
    labelItems() contains logic for dynamically labelling the buttons just like
    a handheld calculator.
    
    Added some flex box styling in createButtonGrid so that the button labels are
    centered when they are added.
    
    Styled the operator buttons differently to add a different visual esthaetic

--- indexScript.js
+++ indexScript.js
@@ -20,13 +20,45 @@ function createButtonGrid(){
                                         .width.split("%");
 
             button.style.width = `${parseInt(cleanedParentWidth[0], 10) / 4}%`;
-            button.style.height = "30px";
-            button.style.border = "1px solid black";
+            button.style.height = "55px";
             button.style.margin = "5px";
-            button.style.borderRadius = "50%";
-            
+            button.style.backgroundColor = "#c0c0c0";
+            button.style.display = "flex";
+            button.style.alignItems = "center";
+            button.style.justifyContent = "center";
+            if (btnBlock === NUMBER_OF_COLUMNS - 1) {
+                button.style.borderRadius = "50%";
+                button.style.backgroundColor = "black";
+            }
         }
         
     }
 }
+
+function labelTheButtons(listOfButtons) {
+    const labels = [
+                    ["7", "8", "9", "+"],
+                    ["4", "5", "6", "-"],
+                    ["1", "2", "3", "x"],
+                    ["", "0", ".", "/"]
+                ];
+    
+    for (let i = 0; i < listOfButtons.length; i++) {
+        
+        let btnList = listOfButtons[i].querySelectorAll(".inputButton");
+        
+        for (let j = 0; j < btnList.length; j++) {
+            let btnLabel = document.createElement("h2");
+            btnLabel.textContent = `${labels[i][j]}`;
+
+            //Different styling for the operator labels
+            if (j === btnList.length - 1)
+                btnLabel.style.color = "white";
+
+            btnList[j].append(btnLabel);
+        }
+    }
+}
+
 createButtonGrid();
+labelTheButtons(document.querySelector(".inputArea").querySelectorAll(".buttonsRow"));
